1f2517294c4aff9780a09f989deef43d0af080a7
Khang Le <98366987+Khangxlei@users.noreply.github.com>
2024-03-07 20:04:00 -0500

    added models table

--- schema.sql
+++ schema.sql
@@ -1,14 +1,22 @@
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS labels;
+DROP TABLE IF EXISTS models;
 
 CREATE TABLE images (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    filename TEXT
+    filename TEXT,
+    image_data BLOB
 );
 
 CREATE TABLE labels (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    image_name TEXT,
-    label TEXT,
-    FOREIGN KEY (image_name) REFERENCES images (filename)
+    image_id INTEGER,
+    label INTEGER,
+    FOREIGN KEY (image_id) REFERENCES images (id)
+);
+
+CREATE TABLE models (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    model_name TEXT,
+    model_file_path TEXT
 );
