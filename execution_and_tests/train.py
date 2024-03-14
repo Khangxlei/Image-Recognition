7fba877351b7d50413a53f9afb4df53356190951
Khang Le <98366987+Khangxlei@users.noreply.github.com>
2024-03-13 23:09:40 -0400

    modified train.py

--- execution_and_tests/train.py
+++ execution_and_tests/train.py
@@ -4,9 +4,9 @@ import json
 # Define the URL of your API
 BASE_URL = 'http://127.0.0.1:5000'  # Assuming your API is running locally on port 5000
 
-def train_model():
+def train_model(parameters, ids):
     # Make a POST request to the /train endpoint
-    response = requests.post(f"{BASE_URL}/train")
+    response = requests.post(f"{BASE_URL}/train", json=parameters, params=ids)
     
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
@@ -22,4 +22,17 @@ def train_model():
         print(f"Error: Unable to train model. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    train_model()
+    parameters = {
+    'epochs': 10,
+    'model_filename': 'model1.h5',
+    'loss_name': 'sparse_categorical_crossentropy',
+    'img_height': 32,
+    'img_width': 32
+    }
+
+    user_id = 'khangxlei'
+    data_id = 'cifar10'
+
+    ids = {'user_id': user_id, 'data_id': data_id}
+
+    train_model(parameters, ids)
