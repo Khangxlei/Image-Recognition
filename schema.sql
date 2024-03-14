0effee5faf7ab5c2b2110595e93023e192c1eccf
Khang Le <98366987+Khangxlei@users.noreply.github.com>
2024-03-13 23:06:45 -0400

    added table for models and table for training

--- schema.sql
+++ schema.sql
@@ -1,22 +1,39 @@
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS labels;
+DROP TABLE IF EXISTS trains;
 DROP TABLE IF EXISTS models;
 
 CREATE TABLE images (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    data_id TEXT,
     filename TEXT,
     image_data BLOB
 );
 
 CREATE TABLE labels (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    data_id TEXT,
     image_id INTEGER,
     label INTEGER,
     FOREIGN KEY (image_id) REFERENCES images (id)
 );
 
+CREATE TABLE trains (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trained_model_file_path TEXT,
+    loss REAL,
+    accuracy REAL,
+    optimizer_type TEXT,
+    lr REAL,
+    momentum REAL,
+    epochs INTEGER,
+    train_time_secs REAL,
+    train_time_mins REAL
+);
+
 CREATE TABLE models (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    model_name TEXT,
-    model_file_path TEXT
+    model_path TEXT
 );
