4461a137457cce015d7f4ad678fbe7ed0c82ce89
Khang Le <98366987+Khangxlei@users.noreply.github.com>
2024-04-11 15:30:26 -0400

    merged upload images and labels

--- modules/upload_data.py
+++ modules/upload_data.py
@@ -4,17 +4,22 @@ from werkzeug.utils import secure_filename
 import json
 import logging
 
-from modules.database import save_image_to_db
+from modules.database import save_image_to_db, save_labels_to_db
 
 upload_images = Blueprint('upload_images', __name__)
+upload_labels = Blueprint('upload_labels', __name__)
 
 # Set the upload folder and allowed extensions
 IMAGE_UPLOAD_FOLDER = 'image_uploads'
+LABELS_UPLOAD_FOLDER = 'label_uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 upload_images.config = {}
+upload_labels.config = {}
 
 logging.basicConfig(filename='upload_images.log', level=logging.DEBUG)
+logging.basicConfig(filename='upload_labels.log', level=logging.DEBUG)
+
 
 @upload_images.record
 def record_params(setup_state):
@@ -63,3 +68,38 @@ def upload_image():
     else:
         logging.error('Invalid file format')
         return jsonify({'error': 'Invalid file format', 'status_code': 400}), 400
+
+
+@upload_labels.record
+def record_params(setup_state):
+    app = setup_state.app
+    upload_labels.config['LABELS_UPLOAD_FOLDER'] = app.config.get('LABELS_UPLOAD_FOLDER', LABELS_UPLOAD_FOLDER)
+
+@upload_labels.route('/upload_labels', methods=['POST'])
+def upload_label():
+    data = request.json
+    logging.debug(f'this is data:{data}')
+
+    user_id = request.args.get('user_id')
+    data_id = request.args.get('data_id')
+    
+    logging.debug(f'this is user_id:{user_id}')
+    
+    # Parse the JSON string to get image names and labels
+    for image_name, label in data.items():
+        save_labels_to_db(user_id, data_id, image_name, label)
+
+    folder_path = upload_labels.config['LABELS_UPLOAD_FOLDER']
+
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    # Specify the file path
+    file_path = os.path.join(folder_path, 'labels.json')
+    
+
+    with open(file_path, 'w') as f:
+        json.dump(data,f)
+
+    return jsonify({'message': 'Labels uploaded succesfully', 'status_code': 200}), 200
